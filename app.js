50f181d2fdd668d8992c9061e7fa1f8d852aab31
Nico Salvador <nico.salvador@bax-shop.nl>
2020-02-25 18:30:37 +0800

    Add .env test

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require('http')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: ".env" })
 const { PORT = 3000 } = process.env
 
+const greeting = process.env.TEST_VALUE || "Howdy (.env file not working)"
+
 http.createServer((req, res) => {
-    res.end('Hello World from Node.js\n')
+    res.end(greeting + ' from Node.js\n')
 }).listen(PORT)
